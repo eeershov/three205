f7288933f179c4922a739adfd50f279f97f4bdd3
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-12-13 19:15:57 +0300

    feature: forms and css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,51 @@
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleEmailInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleNumberInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(email);
+    console.log(number);
+  };
 
   return (
     <>
-      <div>
-        <p>Hello</p>
-        <img src={reactLogo} className='logo' alt='react logo'/>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter e-mail*
+          <input
+            type="text"
+            value={email}
+            onChange={handleEmailInput}
+            placeholder="ivanov@gmail.com"
+            required
+          />
+        </label>
+        <label className='number'>
+          Enter number
+          <input
+            type="text"
+            value={number}
+            onChange={handleNumberInput}
+            placeholder="22-22-22"
+          />
+        </label>
+        <button type="submit">SUBMIT</button>
+        <span className='legend'>* Required fields</span>
+      </form>
     </>
-  )
+  );
 }
 
 export default App
