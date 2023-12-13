7b5a32fdab7b875cf9ddf1d1456d95d8f161fe83
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-12-13 17:29:17 +0300

    Basic project structure

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello</p>
+        <img src={reactLogo} className='logo' alt='react logo'/>
+      </div>
+    </>
+  )
+}
+
+export default App
