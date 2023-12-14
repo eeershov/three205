123ce91ca9e9989b60ba0b8112bdcc787ef69377
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-12-14 18:24:19 +0300

    backend refactor: more structure

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,59 +1,8 @@
-import { Router, Request, Response } from "express";
-import { User, ApiResponse } from '../interfaces';
-import fs from 'fs';
+import { Router } from "express";
+import { getUsers } from "../controllers/getUsers";
 
 const router = Router();
-const RESPONSE_DELAY = 5000; // 5000 in prod
 
-// fast sync file read and type convertion
-const JSON_DATA: Record<keyof User, string>[] = await JSON.parse(
-  fs.readFileSync('./data.json', 'utf8')); 
-const USERS_DATA: User[] = JSON_DATA.map(user => {
-  return {
-    email: user.email,
-    number: parseInt(user.number)
-  }
-})
-
-
-let timeoutID: NodeJS.Timeout | null = null;
-async function getUsers(req: Request, res: Response) {
-  if (timeoutID) clearTimeout(timeoutID); // cancel ongoing response
-  const email = req.query.email;
-  let number: number | null = null;
-  if (req.query.number) number = parseInt(req.query.number.toString());
-
-  const foundUsers = USERS_DATA.filter(user => {
-    if (number != null) {
-      return user.email === email && user.number === number;
-    }
-      return user.email === email;
-  })
-
-  let response: ApiResponse<User[]>;
-  if (foundUsers.length === 0) {
-    response = new ApiResponse({
-      success: false,
-      code: 200, // as not to process the 204 code separately on the client side
-      error: true,
-      message: 'users not found',
-      data: []
-    });
-  } else {
-    response = new ApiResponse({
-      success: true,
-      code: 200,
-      error: false,
-      message: 'users found',
-      data: foundUsers
-    });
-  }
-
-  timeoutID = setTimeout(() => {
-    res.status(response.code).send(response);
-  }, RESPONSE_DELAY
-  );
-}
 
 router.get('/users', getUsers);
 
